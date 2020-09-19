de78a92b46d3d796d577de8f00cad92f08a72c69
Mark Hario <hario.mark@gmail.com>
2020-09-19 15:54:28 -0600

    Fix for cors, save status

--- routes/status.js
+++ routes/status.js
@@ -1,13 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+const fs = require('fs');
+
 router.get('/getAll', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.end('respond with a resource');
+  fs.readFile('status.txt', 'utf-8', (err, status) => {
+    if(! status.length) return res.end(null);
+    res.send( JSON.parse(status) )
+  });
 });
 
-router.post('/set', function(req,res, next) {
-  res.send('doin it!');
+router.post('/set', async function(req,res, next) {
+  let newStatus = req.body;
+  console.log("newStatus", newStatus);
+
+  fs.readFile('status.txt', 'utf-8', (err, status) => {
+    if( ! status ) {
+      newStatus = [ newStatus ];
+    } else {
+      const latestStatus = JSON.parse(status);
+      newStatus = latestStatus.concat([newStatus]);
+    }
+
+    console.log("newStatus", newStatus);
+
+    fs.writeFile('status.txt', JSON.stringify(newStatus), (err,data) => {
+      if (err) throw err;
+      res.sendStatus(201)
+    })
+  });
+
 });
 
 
